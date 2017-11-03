e258bec96063983006ff01b6c335e22dc4c387fe
katm201 <katm201@gmail.com>
2017-11-03 16:51:43 -0700

    Stubbing out solution approach.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,9 +7,14 @@ module.exports = {
   },
 
   users: {
-    // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    
+    get: function (req, res) {
+      // this function handles a request for all posts from a particular user
+    },
+    
+    post: function (req, res) {
+      // this function handles a request to...add a new user?
+    }
   }
 };
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,18 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      // a function which produces all the messages
+      // 1) pulls the data from our database
+      // 2) call the callback
+      
+    }, 
+    post: function () {
+    // a function which adds a new message to our SQL database
+    // 1) collect the incoming message data into a variable
+    
+      // 2) call INSERT INTO (tablename) VALUES (user, room, message)
+    }
   },
 
   users: {
