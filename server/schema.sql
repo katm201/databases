c8e790fd75556e6bacb314f3eca9d9a1bd74dec3
katm201 <katm201@gmail.com>
2017-11-03 15:48:41 -0700

    Added client file and started SQL schema.

--- server/schema.sql
+++ server/schema.sql
@@ -4,12 +4,27 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  room INTEGER,
+  user INTEGER,
+  message_text TEXT,
+  -- created_at DATETIME,
+  FOREIGN KEY(room) REFERENCES roomnames(id),
+  FOREIGN KEY(user) REFERENCES usernames(id)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE roomnames (
+  /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
 
-
-
+CREATE TABLE usernames (
+  /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
